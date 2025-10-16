13da87111cc166f2cb97a9759ce87be244438c68
rohitreapen <rohitreapen@gmail.com>
2025-10-16 09:47:17 +0530

    notes after session 11

--- cplusplus/programs/142_write_trunc.cpp
+++ cplusplus/programs/142_write_trunc.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 int main() {
     std::ofstream out("out1.txt", std::ios::out | std::ios::trunc);
-    if (!out) { std::cerr << "open fail\n"; return 1; }
+    if (!out) {
+        std::cerr << "open fail\n";
+        return 1;
+    }
     out << "Hello file!\n";
 }
